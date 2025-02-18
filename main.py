63c08196df91517c327aecda9b26cc4186b02fb9
falcaoNeto <falcao.coke@gmail.com>
2025-02-18 17:28:18 -0300

    update main

--- main.py
+++ main.py
@@ -28,5 +28,6 @@ app.register_blueprint(carro_route)
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa a porta fornecida pelo Render
+    app.run(host="0.0.0.0", port=port)
 
